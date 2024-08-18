deda2fd5be410ed5f474423ed14121920d6bd5e5
sali200 <ramaz.moseshvili@gmail.com>
2024-08-19 00:46:04 +0400

    commit

---
+++ level 26/classwork/index.py
@@ -0,0 +1,40 @@
+#0 module 1
+print(24*60*60)
+#1
+price=('87$')
+print(price)
+#2
+game=("matser")
+score=99
+print(game)
+print(score)
+#3
+one=("*")
+three=("***")
+four=("****")
+three2=("***")
+one4=("*")
+print(   one)
+print(        three)
+print(             four)
+print(       three2)
+print(     
+    one4) 
+#4 (module 2)
+country=("portugalia")
+capitan=("lisbon")
+print(country,capitan)
+
+
+#6 (module 2)
+balance=int(input("how mutch u nearly have?"))
+cost=40
+print(balance-cost)
+#7 (module 3)
+age=input("how old are u??")
+print(age==19)
+#8   module4
+alfa=["A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z."]
+print(alfa[0]) 
+
+
