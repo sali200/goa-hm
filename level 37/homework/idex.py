2d3957ea7fb6946228cb0b47a08d8cf058668186
sali200 <ramaz.moseshvili@gmail.com>
2024-09-14 00:29:54 +0400

    commit

---
+++ level 37/homework/idex.py
@@ -0,0 +1,12 @@
+#name=input(enter name)
+#place=input(place)
+#print("welcome to the wonderland alice")
+
+
+
+#fruits=[,'apple','cherry','pineapple','orange']
+y= "The quick brown fox jumps over the lazy dog."
+x = y.split(   ' ' )
+
+print(x)
+
