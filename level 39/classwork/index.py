69fa998ab360f3e928d882d70a96fce617b7dea3
sali200 <ramaz.moseshvili@gmail.com>
2024-09-20 22:20:23 +0400

    commit

---
+++ level 39/classwork/index.py
@@ -0,0 +1,4 @@
+
+def calculate (a,b):
+    print(calculate)
+    calculate=9189,8210
---
+++ level 40/homework/index.py
@@ -0,0 +1,20 @@
+# task name "great me"
+name=input('name')
+
+print('Hello,',name)
+# task name "bumps in the road"
+
+def bumps_in_the_road(n):
+    return "Woo-hoo!" * n
+
+print(bumps_in_the_road(3))
+#task name "sum even numbers"
+D=["1", "2", "3","4","5 ", "6","7","8","9"]
+print(D[0]+D[1]+D[2]+D[3]+D[4]+D[5]+D[6]+D[7]+D[8])
+#task name "rverse"
+
+def reverse(s):
+    return s[::-1]
+
+print(reverse(D))
+
