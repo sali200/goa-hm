7cc1176e800962e8746ddbb66d3803b061434487
sali200 <ramaz.moseshvili@gmail.com>
2024-10-30 01:57:05 +0400

    commit

---
+++ level 45/classwork/index.py
@@ -0,0 +1,28 @@
+# Get customer name from user input
+customer_name = input()
+Bill=input("enter bill")
+# Call the printBill function with the customer name
+print(Bill(customer_name))
+
+
+
+def welcome(name):
+    print(f"Welcome, {name}")
+    # Example usage
+welcome("Tommy")  # This will print: Welcome, Tommy
+
+
+
+# Function to calculate the area of a rectangle
+def area(x, y):
+    return x * y
+
+# Get width and height from user input
+w = int(input())
+h = int(input())
+
+# Call the function and print the area
+print(area(w, h))
+
+
+# 0+1+2+3=6
