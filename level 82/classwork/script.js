bb46c3e1a45d8459e920315300e04b529b22ecd5
sali200 <ramaz.moseshvili@gmail.com>
2025-01-19 12:13:15 +0400

    commit

---
+++ level 82/classwork/script.js
@@ -0,0 +1,10 @@
+
+const currentYear = 2025;
+
+
+const age = prompt("შეიყვანეთ თქვენი ასაკი:");
+
+const birthYear = currentYear - age;
+
+
+console.log("თქვენი დაბადების წელი არის: " + birthYear);
